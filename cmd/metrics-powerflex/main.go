3df8ca40a35f0cc76f25313470b5381dd7d014c7
panigs7 <92028646+panigs7@users.noreply.github.com>
2025-08-07 10:56:13 +0530

    Bugfix set log level (#268)

--- cmd/metrics-powerflex/main.go
+++ cmd/metrics-powerflex/main.go
@@ -56,7 +56,6 @@ func main() {
 
 func configure() (*entrypoint.Config, otlexporters.Otlexporter, *service.PowerFlexService) {
 	logger := setupLogger()
-	loadConfig(logger)
 	configFileListener := setupConfigFileListener()
 	sdcFinder, storageClassFinder, leaderElectorGetter, volumeFinder, nodeFinder, exporter := initializeComponents(logger)
 	config := setupConfig(sdcFinder, storageClassFinder, leaderElectorGetter, volumeFinder, nodeFinder, logger)
@@ -90,6 +89,7 @@ func initializeComponents(logger *logrus.Logger) (*k8s.SDCFinder, *k8s.StorageCl
 
 func setupLogger() *logrus.Logger {
 	logger := logrus.New()
+	loadConfig(logger)
 	updateLoggingSettings(logger)
 	return logger
 }
